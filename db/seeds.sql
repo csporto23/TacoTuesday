8e733fca219eec68f5d589d03c8662f31cb762b0
csporto23 <Csporto23@gmail.com>
2019-06-05 16:38:59 -0500

    msql db data added

---
+++ db/seeds.sql
@@ -0,0 +1,1 @@
+INSERT INTO tacos (taco_name) VALUES ('chicken taco');
---
+++ db/tacos_db.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE tacos_db;
+USE tacos_db;
+
+CREATE TABLE tacos (
+id int NOT NULL AUTO_INCREMENT,
+taco_name VARCHAR(255) NOT NULL,
+shell VARCHAR(200) NOT NULL,
+vegetarian BOOLEAN DEFAULT FALSE,
+picked_up BOOLEAN DEFAULT FALSE,
+PRIMARY KEY (id)
+);
