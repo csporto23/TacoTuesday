bf2d25b038c55561a9ac4c7f06ab126f8826c50e
csporto23 <Csporto23@gmail.com>
2019-06-06 01:32:31 -0500

    updated db

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO tacos (taco_name, shell, vegetarian, picked_up) VALUES ('chicken taco', 'floor', false, false);
-INSERT INTO tacos (taco_name, shell, vegetarian, picked_up) VALUES ('beef taco', 'corn', false, false);
-INSERT INTO tacos (taco_name, shell, vegetarian, picked_up) VALUES ('vegan taco', 'corn', true, false);
-
+USE taco_db;
+INSERT INTO tacos (taco_name, shell, vegetarian, picked_up) VALUES ('chicken taco', true, false, false);
+INSERT INTO tacos (taco_name, shell, vegetarian, picked_up) VALUES ('beef taco', true, false, false);
+INSERT INTO tacos (taco_name, shell, vegetarian, picked_up) VALUES ('vegan taco', true, true, false);
---
+++ public/assets/js/tacos.js
@@ -0,0 +1,53 @@
+$(function () {
+    $(".change-pickedUp").on("click", function (event) {
+      var id = $(this).data("id");
+      var newPickedUp = $(this).data("newpickedUp");
+  
+      var newPickedUpState = {
+        picked_up: newPickedUp
+      };
+  
+      $.ajax("/api/tacos/" + id, {
+        type: "PUT",
+        data: newPickedUpState
+      }).then(
+        function () {
+          console.log("changed picked up to", newpickedUp);
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function (event) {
+      event.preventDefault();
+  
+      var newTaco = {
+        taco_name: $("#ta").val().trim(),
+        shell: $("[name=corn]:checked").val().trim(),
+        vegetarian: $("[name=vegetarian]:checked").val().trim(),
+      };
+  
+      $.ajax("/api/tacos", {
+        type: "POST",
+        data: newTaco
+      }).then(
+        function () {
+          console.log("created new taco");
+          location.reload();
+        }
+      );
+    });
+  
+    $(".delete-taco").on("click", function (event) {
+      var id = $(this).data("id");
+  
+      $.ajax("/api/tacos/" + id, {
+        type: "DELETE"
+      }).then(
+        function () {
+          console.log("deleted taco", id);
+          location.reload();
+        }
+      );
+    });
+  });
