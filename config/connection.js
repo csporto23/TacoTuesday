6a1b9d097f56300cf6e9459199fc782f88473c80
csporto23 <Csporto23@gmail.com>
2019-06-05 23:34:20 -0500

    jawsdb fix

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,10 @@ require("dotenv").config();
 const sqlUser = keys.mysql.user
 const sqlPassword = keys.mysql.password
 
-
-const connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
@@ -14,6 +16,8 @@ const connection = mysql.createConnection({
     database: 'tacos_db'
 
 });
+};
+
 
 connection.connect(function(err) {
     if (err) {
